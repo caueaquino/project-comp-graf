b973b98708f08f02445a861608716add1d6b645a
Caue Aquino Nogueira <cauenogueira@alunos.utfpr.edu.br>
2020-10-16 02:16:09 -0300

    finaliza projeto comp grafica

---
+++ Assets/comprimido.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comprimido : MonoBehaviour
+{
+    float unitX, unitZ;
+    float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = 6.0f;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unitX = Input.GetAxis("Horizontal") * v * Time.deltaTime;
+        unitZ = Input.GetAxis("Vertical") * v * Time.deltaTime;
+        transform.Translate(unitX, 0, unitZ);
+    }
+}
---
+++ Assets/tomar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tomar : MonoBehaviour
+{
+
+    float unitX, unitY;
+    float v;
+    public GameObject pers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v = 50f;
+        unitX = 0f;
+        Cursor.lockState = CursorLockMode.Locked;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unitX += Input.GetAxis("Mouse Y") * v * Time.deltaTime;
+        unitX = Mathf.Clamp(unitX, -60f, 60f);
+        transform.localEulerAngles = new Vector3(-unitX, 0, 0);
+        unitY += Input.GetAxis("Mouse X") * v * Time.deltaTime;
+        pers.transform.Rotate(0, unitY, 0);
+    }
+}
